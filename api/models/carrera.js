cc9c381d31061f2e47c396a812a2eb46aa382831
marianoEG <marianen@live.com.ar>
2022-11-01 01:34:19 -0300

    2da iteracion

--- api/models/carrera.js
+++ api/models/carrera.js
@@ -8,5 +8,16 @@ module.exports = (sequelize, DataTypes) => {
     {}
   );
 
+  //codigo de asociacion  (tiene muchos:)
+  carrera.associate = function (models) {
+    carrera.hasMany(
+      models.materia, // Modelo al que pertenece
+      {
+        as: "materia", // nombre de mi relacion
+        foreignKey: "id_carrera", // campo con el que voy a igualar
+      }
+    );
+  };
+
   return carrera;
 };
