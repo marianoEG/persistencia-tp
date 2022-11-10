a689efb97055198196c6b303b8e8937347a7cdc4
JulianPaez92 <jepaez92@gmail.com>
2022-11-10 11:44:17 -0300

    jwt generado

--- api/app.js
+++ api/app.js
@@ -11,6 +11,79 @@ var inscripcionesRouter = require("./routes/inscripciones"); //Agregado inscripc
 
 var app = express();
 
+//Login con JWT
+
+const jwt = require('jsonwebtoken');
+
+const keys = require('./settings/keys');
+const { decode } = require("punycode");
+
+app.set('key', keys.key);
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+  res.send('HOLA MUNDO')
+})
+
+app.post('/login', (req,res)=>{
+  if(req.body.usuario == 'admin' && req.body.pass == '12345'){
+    const payload = {
+      check:true
+    };
+    const token = jwt.sign(payload, app.get('key'),{
+      expiresIn:'7d'
+    });
+    res.json({
+      message:'¡AUTENTICACIÓN EXITOSA!',
+      token: token
+    });
+  }else{
+    res.json({
+      message:'Usuario y/o password son incorrectos'
+    })
+  }
+});
+
+const verificacion = express.Router();
+
+verificacion.use((req, res, next)=>{
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  //console.log(token);
+  if(!token){
+    res.status(401).send({
+      error: 'Es necesario un token de autenticación'
+    })
+    return
+  }
+  if(token.startsWith('Bearer ')){
+    token = token.slice(7, token.length);
+    console.log(token);
+  }
+  if(token){
+    jwt.verify(token, app.get('key'), (error, decoded)=>{
+      if(error){
+        return res.json({
+          message:'El token no es válido'
+        });
+      }else{
+        req.decoded = decoded;
+        next();
+      }
+    })
+  }
+
+});
+
+app.get('/info', verificacion, (req,res)=>{
+  res.json('INFORMACIÓN IMPORTANTE ENTREGADA');
+})
+
+
+//app.listen(3001, ()=>{
+//  console.log('Servidor UP en http://localhost:3000');
+// })
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
